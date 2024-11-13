b75b263289a8b28ecbf2376fadd3d6d0ae80be63
Nathaniel (Binkersss) Chappelle <nathaniel.chappelle@proton.me>
2024-11-13 11:52:14 -0800

    Initial commit: Merge sort implement

---
+++ merg.py
@@ -0,0 +1,6 @@
+import mergesort
+
+test_array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = mergesort.sort(test_array)
+print(f"Original array: {test_array}")
+print(f"Sorted array: {sorted_array}")
